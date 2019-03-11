627508c7fabfe3335f148f888c90ad1d7e0d5b4c
Dan Uhl <duhl@wayfair.com>
2019-03-11 14:20:22 -0400

    adds clicks by week graph

--- src/Stats.js
+++ src/Stats.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import { VictoryLine, VictoryChart, VictoryTheme } from 'victory';
 
+const LINE_CHART_STYLES = {
+  data: { stroke: '#c43a31' },
+  parent: { border: '1px solid #ccc' }
+};
+
+const getAllClicksData = async () => {
+  const response = await fetch(`/.netlify/functions/get-clicks`, {
+    method: 'POST'
+  });
+  const result = response.json();
+  return result;
+};
+
+const getWeekNumber = timestamp => {
+  var d = new Date(timestamp);
+  var dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
+};
+
+const getClicksByWeek = rawData => {
+  // get clicks by week
+  return rawData
+    .sort(({ date: d1 }, { date: d2 }) => parseInt(d1, 10) - parseInt(d2, 10))
+    .reduce((acc, { hash, date }) => {
+      const week = getWeekNumber(parseInt(date, 10));
+      const foundEntry = acc.find(entry => entry.x === week);
+      if (foundEntry) {
+        foundEntry.y++;
+      } else {
+        acc = [...acc, { x: week, y: 1 }];
+      }
+      return acc;
+    }, []);
+};
 export default class Stats extends Component {
+  componentWillMount() {
+    // grab the data
+    getAllClicksData().then(({ results }) => {
+      this.setState({ raw: results, clicksByWeek: getClicksByWeek(results) });
+    });
+  }
+
+  state = {
+    raw: [],
+    clicksByWeek: []
+  };
+
   render() {
     return (
       <VictoryChart theme={VictoryTheme.material}>
-        <VictoryLine
-          style={{
-            data: { stroke: '#c43a31' },
-            parent: { border: '1px solid #ccc' }
-          }}
-          data={[{ x: 1, y: 2 }, { x: 2, y: 3 }, { x: 3, y: 5 }, { x: 4, y: 4 }, { x: 5, y: 7 }]}
-        />
+        <VictoryLine style={LINE_CHART_STYLES} data={this.state.clicksByWeek} />
       </VictoryChart>
     );
   }
---
+++ src/lambda/get-clicks.js
@@ -0,0 +1,55 @@
+import dotenv from 'dotenv';
+import dynamoose from 'dynamoose';
+// this lets us use async await without try catch in a lambda
+import makeSafeAsyncLambdaHandler from '../async-lambda-handler-maker';
+
+dotenv.config({
+  path: `.env`
+});
+
+if (!process.env.AWS_SECRET_ACCESS_KEY_LOCAL || !process.env.AWS_ACCESS_KEY_ID_LOCAL) {
+  throw new Error(
+    'Missing aws environment variables (.env file): AWS_SECRET_ACCESS_KEY_LOCAL, AWS_ACCESS_KEY_ID_LOCAL'
+  );
+}
+
+dynamoose.AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_LOCAL,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_LOCAL,
+  region: 'us-east-1'
+});
+
+const UrlsTable = dynamoose.model('urls', {
+  hash: String,
+  url: String,
+  date: String
+});
+
+const ClicksTable = dynamoose.model('clicks', {
+  id: String,
+  date: String,
+  hash: String
+});
+
+export const handler = makeSafeAsyncLambdaHandler(async function(event, context) {
+  if (event.httpMethod !== 'POST' || !event.body) {
+    return { statusCode: 410, body: JSON.stringify({ msg: 'Unsupported Request Method' }) };
+  }
+
+  // get all rows from the dynamo db
+  const allUrls = await UrlsTable.scan().exec();
+  const allClicks = await ClicksTable.scan().exec();
+
+  const urlsHash = allUrls.reduce((acc, { url, date, hash }) => {
+    return { ...acc, [hash]: url };
+  }, {});
+  // build an array sorted by date
+  const results = allClicks.reduce((acc, { hash, date }) => {
+    return [...acc, { hash, url: urlsHash[hash], date }];
+  }, []);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ results })
+  };
+});
